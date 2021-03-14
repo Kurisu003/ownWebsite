0149bfdb36ca008d2cb6f682352140732191b8da
Kurisu003 <74920220+Kurisu003@users.noreply.github.com>
2021-03-14 09:59:14 +0100

    Updated Qualifications
    Updated Qualifications and made the code for it a lot shorter and cleaner. Also made it easier to create new elements

---
+++ src/Views/Assets/Qualifications/QPanelC.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { useState } from "react";
+
+function QPanelC(props) {
+    
+    const [isOpen, setIsOpen] = useState(false)
+    let classNameSuffix = ''
+    if(isOpen){
+        classNameSuffix = ' Extended' 
+    }
+    else{
+        classNameSuffix = ''
+    }
+    let imgsrc = require("../Svgs/" + props.logoName);
+    // console.log(className)
+
+    return (
+    <div style={{minHeight: 110}}>
+        <div
+            className={'QH' + (isOpen ? classNameSuffix + 'QH' : '')}
+            onClick={() => setIsOpen(!isOpen)}
+        >
+        
+        <img src={imgsrc.default} alt='logo' style={{width: 50, marginLeft: 20, userSelect: 'none'}}/>
+            <p className='QHTitle'>
+                {props.name}
+            </p>
+        </div>
+
+        <div
+            className={'QC' + (isOpen ? classNameSuffix + 'QC' : '')}
+        >
+        {props.buttonInfo.map(element => {
+            return(
+                <div>
+                    <div
+                        className={'QCE' + (isOpen ? classNameSuffix + 'QCE' : '')}
+                    >
+                        
+                        <p
+                            className='QCText'
+                        >
+                            {element[0]}
+                        </p>
+                        <a
+                            className={'QCB' + (isOpen ? classNameSuffix + 'QCB' : '')}
+                            href={element[1]}
+                        >
+                            Github
+                        </a>
+                    </div>
+                </div>
+                
+        )})}
+        </div>
+    </div>
+    );
+}
+
+
+export default QPanelC
--- src/Views/main/Qualifications.js
+++ src/Views/main/Qualifications.js
@@ -1,145 +1,61 @@
 import React from 'react'
 import '../../style/qualifications/qualificationsStyle.css'
+import QPanelC from "../Assets/Qualifications/QPanelC";
 
-import CLogo from '../Assets/Svgs/C.svg'
+function Qualifications() {
 
-function Qualifications(){
 
-    const [isPanelOpen, setIsPanelOpen] = React.useState(false);
-    
-    //used for expansion animation
-    function expansionHandler(attr){
+    let j = [{
+        name: 'Java',
 
-        setIsPanelOpen(!isPanelOpen)
+        logoName: 'JavaLogo.svg',
 
-        document.getElementById(attr).style.transition = "all 1s, borderadius 0s";
-        if(isPanelOpen){
-            document.getElementById(attr + 'Content').style.transition = "all 1s ease 0s"
+        buttonInfo: [
+            ['Tachenrechner','https://github.com'],
+            ['Paketdienst','https://github.com'],
+            ['Tic-Tac-Toe mit KI','https://github.com'],
+        ]
+    }]
 
-            document.getElementById(attr).style.maxWidth = "80%"
-            document.getElementById(attr).style.width = "80%"
-            document.getElementById(attr).style.borderBottomLeftRadius = "0px"
-            document.getElementById(attr).style.borderBottomRightRadius = "0px"
-            
-            document.getElementById(attr + 'Content').style.height = "300px"
-            document.getElementById(attr + 'Content').style.maxWidth = "80%"
-            document.getElementById(attr + 'Content').style.width = "80%"
-            document.getElementById(attr + 'Content').style.borderBottomLeftRadius = "10px"
-            document.getElementById(attr + 'Content').style.borderBottomRightRadius = "10px"
-            
-            document.getElementById(attr + 'ContentElement').style.display = "inline-flex"
-            document.getElementById(attr + 'ContentElement2').style.display = "inline-flex"
-            document.getElementById(attr + 'ContentElement3').style.display = "inline-flex"
+    let c = [{
+        name: 'C',
 
-            document.querySelector('#' + attr + 'Content').addEventListener('transitionend', () => {
-                if(document.getElementById(attr + 'Content').style.height !== "0px"){
-                    console.log('Animation ended');
-                    document.getElementById(attr + 'ContentButton').style.display = "flex"
-                    document.getElementById(attr + 'ContentButton2').style.display = "flex"
-                    document.getElementById(attr + 'ContentButton3').style.display = "flex"
-                }
-            });
+        logoName: 'C.svg',
 
-        }
-        else{
-            document.getElementById(attr + 'Content').style.transition = "all 1.13s"
+        buttonInfo: [
+            ['Shell','https://github.com'],
+            ['Tic-Tac-Toe','https://github.com'],
+        ]
+        
+    }]
 
-            document.getElementById(attr).style.maxWidth = "200px"
-            document.getElementById(attr).style.borderBottomLeftRadius = "10px"
-            document.getElementById(attr).style.borderBottomRightRadius = "10px"
+    let js = [{
+        name: 'Javascript',
 
-            // document.getElementById(attr + 'Content').style.height = "0px"
-            document.getElementById(attr + 'Content').style = "height: 0px"
-            document.getElementById(attr + 'Content').style.width = "0px"
-            document.getElementById(attr + 'Content').style.borderBottomLeftRadius = "0px"
-            document.getElementById(attr + 'Content').style.borderBottomRightRadius = "0px"
+        logoName: 'C.svg',
 
-            document.getElementById(attr + 'ContentElement').style.display = "none"
-            document.getElementById(attr + 'ContentElement2').style.display = "none"
-            document.getElementById(attr + 'ContentElement3').style.display = "none"
+        buttonInfo: [
+            ['Fourier Series','https://github.com'],
+        ]
+        
+    }]
 
-            
-            document.getElementById(attr + 'ContentButton').style.display = "none"
-            document.getElementById(attr + 'ContentButton2').style.display = "none"
-            document.getElementById(attr + 'ContentButton3').style.display = "none"
-
-        }
-    }
+    let array = [j, c, js]
 
     return(
         <div>
-            <div>
-                <div 
-                    className='QualificationsButton'
-                    id='C'
-                    onClick={() => expansionHandler('C')}
-                    >
-                    <img src={CLogo} alt='C logo' style={{width: 50, marginLeft: 20}}/>
-                    <p className='QualificationButtonTitle'>
-                        C
-                    </p>
-                </div>
-
-                <div
-                    id='CContent'
-                    className='QualificationsContent'
-                >
-                    <div id='CContentElement' className='QualificationsContentElement'>
-                        <p 
-                            id='CContentText'
-                            className='QualificationsContentText'
-                        >
-                            Eigene Shell
-                        </p>
-                        <a 
-                            id='CContentButton'
-                            className='QualificationsContentButton'
-                            href='https://github.com'
-                        >
-                            Github
-                        </a>
-                    </div>
-
-                    <br></br>
-                    
-                    <div id='CContentElement2' className='QualificationsContentElement'>
-                        <p 
-                            id='CContentText'
-                            className='QualificationsContentText'
-                        >
-                            Sortier Algorithmen
-                        </p>
-                        <a 
-                            id='CContentButton2'
-                            className='QualificationsContentButton'
-                            href='https://github.com'
-                        >
-                            Github
-                        </a>
-                    </div>
-
-                    <br></br>
-                    
-                    <div id='CContentElement3' className='QualificationsContentElement'>
-                        <p 
-                            id='CContentText3'
-                            className='QualificationsContentText'
-                        >
-                            Binary-Search-Tree
-                        </p>
-                        <a 
-                            id='CContentButton3'
-                            className='QualificationsContentButton'
-                            href='https://github.com'
-                        >
-                            Github
-                        </a>
-                    </div>
-                    
-                </div>
-            </div>
+            
+            {array.map(content => (
+                content.map(item =>(
+                    <QPanelC name={item.name} buttonInfo={item.buttonInfo} logoName={item.logoName}/>
+                ))
+            )) 
+            }
+            <br/>
+            {/* <QTest2></QTest2> */}
         </div>
     )
+
 }
 
 export default Qualifications
